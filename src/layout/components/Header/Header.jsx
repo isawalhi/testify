7df92235ecd57d7398f391bf08d588da8f3fa205
Ibrahim Sawalhi <ibrahimswalhi@gmail.com>
2020-11-29 14:45:21 +0100

    Add back button

--- src/layout/components/Header/Header.jsx
+++ src/layout/components/Header/Header.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import Avatar from 'antd/es/avatar';
 import PageHeader from 'antd/es/page-header';
 import UserOutlined from '@ant-design/icons/UserOutlined';
 
 import styles from './Header.module.css';
 
-const Header = () => (
-  <PageHeader
-    title="Testify"
-    className={styles.pageHeader}
-    extra={<Avatar icon={<UserOutlined />} />}
-  />
-);
+const Header = () => {
+  const history = useHistory();
+
+  const onBackHandler = useCallback(() => {
+    history.goBack();
+  }, [history]);
+
+  return (
+    <PageHeader
+      title="Testify"
+      onBack={onBackHandler}
+      className={styles.pageHeader}
+      extra={<Avatar icon={<UserOutlined />} />}
+    />
+  );
+};
 
 export default Header;
