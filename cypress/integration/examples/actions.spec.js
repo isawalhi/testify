927fd8b0d5670bd96c0dfd737ae9ebe96330f5d6
Ibrahim Sawalhi <ibrahimswalhi@gmail.com>
2021-02-28 18:05:45 +0100

    anit-al

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -3,7 +3,7 @@
 describe('Actions', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.document().then((document) => document.head.insertAdjacentHTML('beforeend', '<style>html { overflow-y: hidden; -webkit-font-smoothing: none; font-smooth: never }</style>'));
+    cy.document().then((document) => document.head.insertAdjacentHTML('beforeend', '<style>html { overflow-y: hidden; -webkit-font-smoothing: antialiased; font-smooth: always }</style>'));
   });
 
   // https://on.cypress.io/interacting-with-elements
