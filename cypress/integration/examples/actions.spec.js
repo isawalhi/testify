3799bde6cfc70a9f53af2f7c5a71308f7b2cb0c1
Ibrahim Sawalhi <ibrahimswalhi@gmail.com>
2021-02-28 17:39:46 +0100

    2 screenshots

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -3,6 +3,7 @@
 describe('Actions', () => {
   beforeEach(() => {
     cy.visit('/');
+    cy.document().then((document) => document.head.insertAdjacentHTML('beforeend', '<style>* { font-family: Helvetica !important; } html { overflow-y: hidden; }</style>'));
   });
 
   // https://on.cypress.io/interacting-with-elements
@@ -11,5 +12,6 @@ describe('Actions', () => {
     // https://on.cypress.io/type
     cy.wait(4000);
     cy.get('.ant-btn').matchImageSnapshot('Landing page button');
+    cy.get('.ant-btn').matchImageSnapshot('Landing page button 1');
   });
 });
