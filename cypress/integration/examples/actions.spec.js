dd756b0141349d8c2759921ecacdb8810ee15015
Ibrahim Sawalhi <ibrahimswalhi@gmail.com>
2021-02-28 11:33:14 +0100

    test button

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,16 +1,15 @@
 /// <reference types="cypress" />
 
-context('Actions', () => {
+describe('Actions', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
   // https://on.cypress.io/interacting-with-elements
 
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
-    cy.wait(4000)
-    cy.get('.ant-btn')
-    cy.matchImageSnapshot("Test");
+    cy.wait(4000);
+    cy.get('.ant-btn').matchImageSnapshot("Test");
   })
 })
