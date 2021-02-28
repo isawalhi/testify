6b35f0070640bbb59c452ddaa9853984d0fbbf38
Ibrahim Sawalhi <ibrahimswalhi@gmail.com>
2021-02-28 16:57:57 +0100

    chromuim

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,6 +11,31 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const chromium = require('chromium');
+const execa = require('execa');
+
+const findBrowser = () => {
+  // the path is hard-coded for simplicity
+  const browserPath = chromium.path;
+
+  return execa(browserPath, ['--version']).then((result) => {
+    // STDOUT will be like "Chromium 77.0.69.135"
+    const [, version] = /Chromium (\d+\.\d+\.\d+\.\d+)/.exec(
+      result.stdout,
+    );
+    const majorVersion = parseInt(version.split('.')[0]);
+
+    return {
+      name: 'chromium',
+      channel: 'stable',
+      family: 'chromium',
+      displayName: 'Chromium',
+      version,
+      path: browserPath,
+      majorVersion,
+    };
+  });
+};
 
 const {
   addMatchImageSnapshotPlugin,
@@ -19,7 +44,20 @@ const {
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+module.exports = async (on, config) => {
+  // config.browsers.push({
+  //   channel: 'stable',
+  //   path: chromium.path,
+  //   name: 'chromium',
+  //   family: 'chromium',
+  //   displayName: 'Fuck',
+  //   version: '88',
+  //   majorVersion: 88,
+  // });
+ await findBrowser().then((browser) => {
+    // console.log(browser);
+    config.browsers.push(browser);
+  });
   on('before:browser:launch', (browser = {}, args) => {
     if (browser.name === 'chrome' || browser.name === 'chromium') {
       // In headless mode, Cypress fixes the scale factor to 1, and this forces
@@ -31,13 +69,7 @@ module.exports = (on, config) => {
       //
       // See: https://github.com/cypress-io/cypress/issues/2102#issuecomment-521299946
       // See: https://github.com/cypress-io/cypress/blame/a7dfda986531f9176468de4156e3f1215869c342/packages/server/lib/cypress.coffee#L132-L137
-      args.push('--force-device-scale-factor=3');
-      // args.push(...['--no-sandbox', '--disable-setuid-sandbox', '--hide-scrollbars', '--font-render-hinting=none']);
-    } else if (browser.name === 'electron' && browser.isHeaded) {
-      // eslint-disable-next-line no-console
-      console.log(
-        "There isn't currently a way of setting the device scale factor in Cypress when running headed electron so we disable the image regression commands."
-      );
+      args.push(...['--force-device-scale-factor=3', '--no-sandbox', '--disable-setuid-sandbox', '--hide-scrollbars', '--font-render-hinting=none']);
     }
 
     return args;
@@ -45,4 +77,5 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   addMatchImageSnapshotPlugin(on, config);
-}
+  return config;
+};
