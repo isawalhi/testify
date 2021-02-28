07f3f437ba7589ac0da320cdf568680781f1939a
Ibrahim Sawalhi <ibrahimswalhi@gmail.com>
2021-02-28 18:44:19 +0100

    use electron

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -54,34 +54,30 @@ module.exports = async (on, config) => {
   //   version: '88',
   //   majorVersion: 88,
   // });
- await findBrowser().then((browser) => {
+  await findBrowser().then((browser) => {
     // console.log(browser);
     config.browsers.push(browser);
   });
-  on('before:browser:launch', (browser = {}, launchOptions) => {
-    if (browser.name === 'chrome' || browser.name === 'chromium') {
-      // In headless mode, Cypress fixes the scale factor to 1, and this forces
-      // screenshots to be taken with an image size matching the viewport size
-      // instead of the viewport size multiplied by the scale factor.
-      //
-      // Since we also want to run the image regression tests in development mode,
-      // we need to set the device scale factor to 1 in chrome / chromium.
-      //
-      // See: https://github.com/cypress-io/cypress/issues/2102#issuecomment-521299946
-      // See: https://github.com/cypress-io/cypress/blame/a7dfda986531f9176468de4156e3f1215869c342/packages/server/lib/cypress.coffee#L132-L137
-      launchOptions.args.push('--force-device-scale-factor=3');
-      launchOptions.args.push(...['--no-sandbox', '--disable-setuid-sandbox', '--hide-scrollbars', '--font-render-hinting=none']);
-    } else if (browser.name === 'electron' && browser.isHeaded) {
-      // eslint-disable-next-line no-console
-      console.log(
-        "There isn't currently a way of setting the device scale factor in Cypress when running headed electron so we disable the image regression commands.",
-      );
-    }
+  on('before:browser:launch', (_browser = {}, launchOptions) => {
+    // In headless mode, Cypress fixes the scale factor to 1, and this forces
+    // screenshots to be taken with an image size matching the viewport size
+    // instead of the viewport size multiplied by the scale factor.
+    //
+    // Since we also want to run the image regression tests in development mode,
+    // we need to set the device scale factor to 1 in chrome / chromium.
+    //
+    // See: https://github.com/cypress-io/cypress/issues/2102#issuecomment-521299946
+    // See: https://github.com/cypress-io/cypress/blame/a7dfda986531f9176468de4156e3f1215869c342/packages/server/lib/cypress.coffee#L132-L137
+    launchOptions.args.push('--force-device-scale-factor=3');
+    launchOptions.args.push('--disable-setuid-sandbox');
+
+    launchOptions.args.push(...['--no-sandbox', '--disable-setuid-sandbox', '--hide-scrollbars', '--font-render-hinting=none', '--enable-font-antialiasing', '--disable-gpu']);
 
     return launchOptions;
   });
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   addMatchImageSnapshotPlugin(on, config);
+
   return config;
 };
